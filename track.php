e83bdd7cbf7e98e852d67760194f2913914d4892
Manoj Kumar <manojkumar2684@gmail.com>
2024-07-30 11:15:19 +0530

    link hits track

---
+++ track.php
@@ -0,0 +1,23 @@
+<?php
+include_once('config.php');
+
+$url = isset($_GET['url']) ? $_GET['url'] : null;
+$date = date('Y-m-d h:i:s');
+
+if(!$url || !filter_var($url, FILTER_VALIDATE_URL)){
+    echo json_encode(['status' => 'failure']);
+    exit;
+}
+
+$sql = "INSERT INTO referrals.track_links (link_text, created) VALUES ('{$conn->real_escape_string($url)}', '$date')";
+
+if($conn->query($sql)){
+    echo json_encode(['status' => 'success']);
+    exit;
+}
+
+$conn->close();
+
+echo json_encode(['status' => 'failure']);
+exit;
+?>
